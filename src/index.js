a85c44236d9c6dd563904f098ce3535b6d4ef7ed
Pranjal Jain <jainpranjal30112002@gmail.com>
2023-12-29 15:32:53 +0530

    database connection added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-const express = require("express");
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.get('/', (req,res) => {
-    res.send('Hello World');
+dotenv.config({
+    path: './env'
 })
 
-app.listen(port, () => {
-    console.log(`App listening on port: ${port}`);
-})
+// const express = require("express");
+// const app = express();
+
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req,res) => {
+//     res.send('Hello World');
+// })
+
+// app.listen(port, () => {
+//     console.log(`App listening on port: ${port}`);
+// })
+ 
+connectDB()
