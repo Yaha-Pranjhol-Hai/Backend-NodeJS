3b9055ba02ddfbc6f2d4b2577837cc772b195b36
Pranjal Jain <jainpranjal30112002@gmail.com>
2024-01-01 22:32:59 +0530

    video and user model are added, token flow remains

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Model } from "mongoose";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true, //whitespaces will be removed
+        index: true //this is expensive but it is needed for effective searching
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, //cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, //cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken: {
+        type: String,
+    }
+},{timestamps: true})
+
+export const User = Model("User", userSchema);
